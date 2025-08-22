ae800185e7543e6e3d8930ad08da0d5cf9bf90a9
Souhail Guobi <guobisouhail@gmail.com>
2025-08-22 12:18:17 -0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -136,7 +136,7 @@ function App() {
     });
   };
 
-  const API_BASE = 'https://test-expo-flow.onrender.com/api';
+  const API_BASE = 'https://aug-21-test-connection.onrender.com/';
 
   const fetchOrdersByBooth = async (boothNum, forceRefresh = false) => {
     setLoading(true);
